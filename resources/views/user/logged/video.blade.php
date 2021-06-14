a73e60deef56166a02c2469b5ce4790496571ded
Mujhtech <mujeeb.muhideen@gmail.com>
2021-06-14 02:01:24 +0100

    ...

--- resources/views/user/logged/video.blade.php
+++ resources/views/user/logged/video.blade.php
@@ -14,47 +14,49 @@
                     <button class="btn btn-success float-right" type="button" data-toggle="modal" data-target="#exampleModal">Add new video</button>
                     <h4 class="card-title mb-3">My Videos</h4>
                     <div class="table-responsive">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Number of views</th>
-                                    <th scope="col">Likes</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                            </thead>
 
-                            <tbody>
-                                @if(count($treads) > 0)
-                                @php $sn = 0 @endphp
-                                @foreach($treads as $tread)
-                                @php $sn++ @endphp
-                                <tr>
-                                    <th scope="row">{{ $sn }}</th>
-                                    <td><a href="{{ route('web.single', $tread->slug) }}">{{$tread->title}}</a></td>
-                                    <td>
-                                        {{$tread->views}}
-                                    </td>
-                                    <td>
-                                        {{$tread->likes}}
-                                    </td>
-                                    <td>
-                                        @if($tread->status == 1)
+
+                    <div class="row gx-3 p-3">
+
+                        @if(count($treads) > 0)
+                        @php $sn = 0 @endphp
+                        @foreach($treads as $tread)
+                        @php $sn++ @endphp
+
+                        <div class="card mb-3 col-xl-6 col-lg-6 col-md-6">
+                            <div class="row requests white-bg  wow fadeInUp2  animated" data-wow-delay=".1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp2;">
+                                <div class="col-md-4 projects__thumb" style="background: url('{{ $tread->featured_image_url }}');  background-repeat: no-repeat; background-size: cover;">
+                                </div>
+                                <div class="col-md-8 projects__content">
+                                    <h4 class="text-primary"><a href="{{ route('web.single', $tread->slug) }}">{{ $sn }}. {{ $tread->title }}</a></h4>
+                                    <div class="skill mb-30">
+                                       
+                                       <p class="text-secondary" >Status<span>: @if($tread->status == 1)
                                         <span class="badge badge-success">Publish</span>
                                         @elseif($tread->status == 2)
                                         <span class="badge badge-success">Draft</span>
                                         @elseif($tread->status == 0)
                                         <span class="badge badge-success">Pending</span>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <button class="btn btn-success" type="button" data-toggle="modal" data-target="#exampleModal{{$tread->id}}"><i class="nav-icon i-Pen-2" title="Edit"></i></button>
-                                        <a href="{{ route('user.v.delete', ['id' => $tread->id]) }}" class="btn btn-danger" type="button"><i class="nav-icon i-Close-Window" title="Delete"></i></a>
-                                    </td>
-                                </tr>
-                                <div class="modal fade" id="exampleModal{{$tread->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        @endif </span></p>
+                                </div>
+                                <div class="projects__content--manager">
+                                    <ul class="request-manager">
+                                        <li><a href="#">
+                                            <img src="{{ $tread->user->profile_photo_url }}" alt="">
+                                            <span>{{ $tread->user->username }}</span></a></li>
+                                            <li>
+                                                <p class="time"><i class="far fa-clock"></i> {{ $tread->created_at->diffForHumans() }}</p>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <button class="btn bg-white _r_btn border-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="_dot _inline-dot bg-primary"></span><span class="_dot _inline-dot bg-primary"></span><span class="_dot _inline-dot bg-primary"></span></button>
+                                    <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 33px, 0px); top: 0px; left: 0px; will-change: transform;"><a class="dropdown-item ul-widget__link--font" type="button" data-toggle="modal" data-target="#exampleModal{{$tread->id}}"><i class="i-Pen-2"> </i> Edit</a><a class="dropdown-item ul-widget__link--font" href="{{ route('user.v.delete', ['id' => $tread->id]) }}"><i class="i-Close-Window"> </i> Delete</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="modal fade" id="exampleModal{{$tread->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                             <form action="" method="POST" enctype="multipart/form-data">
@@ -89,8 +91,14 @@
                                                 </div>
                                                 <div class="row">
                                                     <div class="col-md-12">
-                                                        <p class="font-weight-400 mb-2">Attached File</p>
-                                                        <input class="form-control" type="file" name="attachment"/>
+                                                        <p class="font-weight-400 mb-2">Attached Video</p>
+                                                        <input class="form-control" type="file" name="attachment" accept="video/*" required />
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <p class="font-weight-400 mb-2">Attached Featured Image</p>
+                                                        <input class="form-control" type="file" name="featured_image" accept="image/*" required />
                                                     </div>
                                                 </div>
 
@@ -102,13 +110,16 @@
                                         </form>
                                     </div>
                                 </div>
-                            </div>
-                            @endforeach
-                            @else
-                            <tr><td colspan="6" style="text-align: center;">No data found</td></tr>
-                            @endif
-                        </tbody>
-                    </table>
+
+                    @endforeach
+                    @else
+                    <div class="col-md-12"><h4>No data found</h4></div>
+                    @endif
+
+
+                    </div>
+
+
                 </div>
             </div>
         </div>
