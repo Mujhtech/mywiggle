499b374917ecc5d161f481b19d79a3efccf1a3e1
Mujhtech <mujeeb.muhideen@gmail.com>
2021-07-04 15:13:06 +0100

    ...

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -681,18 +681,20 @@ class AdminController extends Controller
 
     public function editTreads(Request $request){
 
-        for ($i=0; $i < count($request->treads); $i++) { 
+        dd($request->all());
 
-            $t = Tread::find($request->treads[$i]);
-            $t->is_ads = isset($request->ads[$i]) && $request->ads[$i] == 'on' ? 1 : 0;
-            $t->is_sponsored = isset($request->sponsored[$i]) && $request->sponsored[$i] == 'on' ? 1 : 0;
-            $t->is_trending = isset($request->trending[$i]) && $request->trending[$i] == 'on' ? 1 : 0;
-            $t->save();
+        //for ($i=0; $i < count($request->treads); $i++) { 
 
-        }
+            //$t = Tread::find($request->treads[$i]);
+            //$t->is_ads = isset($request->ads[$i]) && $request->ads[$i] == 'on' ? 1 : 0;
+            //$t->is_sponsored = isset($request->sponsored[$i]) && $request->sponsored[$i] == 'on' ? 1 : 0;
+            //$t->is_trending = isset($request->trending[$i]) && $request->trending[$i] == 'on' ? 1 : 0;
+            //$t->save();
+
+        //}
 
 
-        return redirect()->back()->with('success','Changes made successfully');
+        //return redirect()->back()->with('success','Changes made successfully');
 
     }
 
